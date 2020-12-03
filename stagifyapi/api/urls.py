447cf9ab2385196cfa97d6d0f56cc9b92c4f45b8
David <davidhodel6@gmail.com>
2020-12-03 15:16:08 +0100

    added basic authentification endpoints

--- stagifyapi/api/urls.py
+++ stagifyapi/api/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('user/register', views.register, name='register'),
+    path('user/login', views.logIn, name='login'),
+    path('user/logout', views.logOut, name='logout'),
+    path('user', views.userInfo, name='userinfo'),
 ]
--- stagifyapi/api/views.py
+++ stagifyapi/api/views.py
@@ -1,10 +1,60 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseServerError, HttpResponse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+import json
 
 from .models import Question
 
-# Create your views here.
 def index(request):
     firstQuestion = Question.objects.first()
 
     return HttpResponse(firstQuestion.question_text)
+
+def register(request):
+    if request.method == 'POST':
+        request_user = json.loads(request.body)
+        try:
+            username = request_user['username']
+            email = request_user['email']
+            password = request_user['password']
+
+            user = User.objects.create_user(username, email, password)
+            return HttpResponse(user)
+        except KeyError:
+            return HttpResponseServerError("Malformed data!")
+
+    return HttpResponseNotFound()
+
+def logIn(request):
+    if request.method == 'POST':
+        request_user = json.loads(request.body)
+        try:
+            username = request_user['username']
+            password = request_user['password']
+
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return HttpResponse("Authentification was successful")
+
+            return HttpResponse('Invalid credentials supplied', status=401)
+        except KeyError:
+            return HttpResponseServerError("Malformed data!")
+
+    return HttpResponseNotFound()
+
+def logOut(request):
+    if request.method == 'POST':
+        if not request.user.is_authenticated:
+            return HttpResponse('Unauthorized', status=401)
+
+        logout(request)
+        return HttpResponse("Logging out was successful")
+
+    return HttpResponseNotFound()
+
+def userInfo(request):
+    if not request.user.is_authenticated:
+        return HttpResponse('Unauthorized', status=401)
+
+    return HttpResponse(request.user.email)
