ff3db875e1df0b136af6a6d5581193084af0432f
David <davidhodel6@gmail.com>
2020-12-03 20:12:58 +0100

    use decoraters for views instead of custom code for restricting http method

--- stagifyapi/api/views.py
+++ stagifyapi/api/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseServerError, HttpResponse
+from django.http import HttpResponse, HttpResponseServerError, HttpResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
+from django.views.decorators.http import require_http_methods
 import json
 
 from .models import Question
@@ -10,49 +11,44 @@ def index(request):
 
     return HttpResponse(firstQuestion.question_text)
 
+@require_http_methods(["POST"])
 def register(request):
-    if request.method == 'POST':
-        request_user = json.loads(request.body)
-        try:
-            username = request_user['username']
-            email = request_user['email']
-            password = request_user['password']
+    request_user = json.loads(request.body)
+    try:
+        username = request_user['username']
+        email = request_user['email']
+        password = request_user['password']
 
-            user = User.objects.create_user(username, email, password)
-            return HttpResponse(user)
-        except KeyError:
-            return HttpResponseServerError("Malformed data!")
+        user = User.objects.create_user(username, email, password)
+        return HttpResponse(user)
+    except KeyError:
+        return HttpResponseServerError("Malformed data!")
 
-    return HttpResponseNotFound()
-
+@require_http_methods(["POST"])
 def logIn(request):
-    if request.method == 'POST':
-        request_user = json.loads(request.body)
-        try:
-            username = request_user['username']
-            password = request_user['password']
+    request_user = json.loads(request.body)
+    try:
+        username = request_user['username']
+        password = request_user['password']
 
-            user = authenticate(request, username=username, password=password)
-            if user is not None:
-                login(request, user)
-                return HttpResponse("Authentification was successful")
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return HttpResponse("Authentification was successful")
 
-            return HttpResponse('Invalid credentials supplied', status=401)
-        except KeyError:
-            return HttpResponseServerError("Malformed data!")
+        return HttpResponse('Invalid credentials supplied', status=401)
+    except KeyError:
+        return HttpResponseServerError("Malformed data!")
 
-    return HttpResponseNotFound()
-
+@require_http_methods(["POST"])
 def logOut(request):
-    if request.method == 'POST':
-        if not request.user.is_authenticated:
-            return HttpResponse('Unauthorized', status=401)
+    if not request.user.is_authenticated:
+        return HttpResponse('Unauthorized', status=401)
 
-        logout(request)
-        return HttpResponse("Logging out was successful")
+    logout(request)
+    return HttpResponse("Logging out was successful")
 
-    return HttpResponseNotFound()
-
+@require_http_methods(["GET"])
 def userInfo(request):
     if not request.user.is_authenticated:
         return HttpResponse('Unauthorized', status=401)
