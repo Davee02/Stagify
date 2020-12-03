c8179dcfe526ed1a185aa175d80d699fbcb11582
David <davidhodel6@gmail.com>
2020-12-03 20:33:49 +0100

    always return a json response

--- stagifyapi/api/views.py
+++ stagifyapi/api/views.py
@@ -1,56 +1,77 @@
-from django.http import HttpResponse, HttpResponseServerError, HttpResponse
+from django.http import HttpResponse, JsonResponse
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods
+from django.db import IntegrityError
 import json
 
 from .models import Question
 
+
 def index(request):
     firstQuestion = Question.objects.first()
 
-    return HttpResponse(firstQuestion.question_text)
+    return JsonResponse({"question": firstQuestion.question_text})
+
 
 @require_http_methods(["POST"])
 def register(request):
-    request_user = json.loads(request.body)
     try:
+        request_user = json.loads(request.body)
         username = request_user['username']
         email = request_user['email']
         password = request_user['password']
 
         user = User.objects.create_user(username, email, password)
-        return HttpResponse(user)
+        response = {"id": user.id, "email": user.email, "username": user.username}
+        
+        return JsonResponse(response)
     except KeyError:
-        return HttpResponseServerError("Malformed data!")
+        return JsonResponse({"message": "Malformed data!"}, status=400)
+    except IntegrityError as e:
+        return JsonResponse({"message": "An user with the same username already exists"}, status=409)
+    except Exception as e:
+        return JsonResponse({"message": "An unexpected error happened: " + str(e)}, status=500)
+
 
 @require_http_methods(["POST"])
 def logIn(request):
-    request_user = json.loads(request.body)
     try:
+        request_user = json.loads(request.body)
         username = request_user['username']
         password = request_user['password']
 
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
-            return HttpResponse("Authentification was successful")
+            return JsonResponse({"message": "Authentification was successful"})
 
-        return HttpResponse('Invalid credentials supplied', status=401)
+        return JsonResponse({"message": 'Invalid credentials supplied'}, status=401)
     except KeyError:
-        return HttpResponseServerError("Malformed data!")
+        return JsonResponse({"message": "Malformed data!"}, status=400)
+    except Exception as e:
+        return JsonResponse({"message": "An unexpected error happened: " + str(e)}, status=500)
+
 
 @require_http_methods(["POST"])
 def logOut(request):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    try:
+        if not request.user.is_authenticated:
+            return JsonResponse({"message": 'Unauthorized'}, status=401)
 
-    logout(request)
-    return HttpResponse("Logging out was successful")
+        logout(request)
+        return JsonResponse({"message": "Logging out was successful"})
+    except Exception as e:
+        return JsonResponse({"message": "An unexpected error happened: " + str(e)}, status=500)
+
 
 @require_http_methods(["GET"])
 def userInfo(request):
-    if not request.user.is_authenticated:
-        return HttpResponse('Unauthorized', status=401)
+    try:
+        if not request.user.is_authenticated:
+            return JsonResponse({"message": 'Unauthorized'}, status=401)
 
-    return HttpResponse(request.user.email)
+        response = {"id": request.user.id, "email": request.user.email, "username": request.user.username}
+        return JsonResponse(response)
+    except Exception as e:
+        return JsonResponse({"message": "An unexpected error happened: " + str(e)}, status=500)
