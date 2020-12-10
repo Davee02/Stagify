9a942fbc5d3189dfa2b94c68230c491aea596006
David <davidhodel6@gmail.com>
2020-12-10 13:24:44 +0100

    deprecate old models and remove test api endpoint

--- stagifyapi/api/urls.py
+++ stagifyapi/api/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import auth, test, concert
 
 urlpatterns = [
-    path('', test.index, name='index'),
     path('user/register', auth.register, name='register'),
     path('user/login', auth.logIn, name='login'),
     path('user/logout', auth.logOut, name='logout'),
--- stagifyapi/api/views/test.py
+++
@@ -1,9 +0,0 @@
-from django.http import JsonResponse
-
-from ..models import Question
-
-
-def index(request):
-    firstQuestion = Question.objects.first()
-
-    return JsonResponse({"question": firstQuestion.question_text})
